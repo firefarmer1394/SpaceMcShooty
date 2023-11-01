b43b41592bae115e9c2a66ee71bb6043a5ea2267
firefarmer1394 <113232108+firefarmer1394@users.noreply.github.com>
2023-11-01 15:39:50 +0100

    Better shiel material, Laser shooting scrip clean up

--- Assets/ProjectSkidbladnir/Scripts/ShootingLaser.cs
+++ Assets/ProjectSkidbladnir/Scripts/ShootingLaser.cs
@@ -5,24 +5,13 @@ public class ShootingLaser : MonoBehaviour
     [SerializeField] GameObject laserBeamPrototype;
     [SerializeField] KeyCode button1, button2;
 
-
-    [SerializeField] float fireRate;
-    [SerializeField] float coolDown;
-    [SerializeField] float overheatRate;
-
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
         if (Input.GetKeyDown(button1) || Input.GetKeyDown(button2))
         {
-            GameObject newGameObject = Instantiate(laserBeamPrototype, transform.position, transform.rotation);
+            Instantiate(laserBeamPrototype, transform.position, transform.rotation);
         }
         //if (coolDown > 0)
         //    coolDown -= Time.deltaTime;
-
     }
 }
